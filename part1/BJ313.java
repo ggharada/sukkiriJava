ca13105a9f923f6e00a16cdc2978e1ba82295677
genki <ggharada39@gmail.com>
2021-05-13 11:11:54 +0900

    part1

---
+++ part1/BJ313.java
@@ -0,0 +1,8 @@
+public class BJ313 {
+    public static void main(String[] args){
+        int age = 20;
+        System.out.println("私の年齢は" + age);
+        age = 31;
+        System.out.println("・・・いや本当の年齢は" + age);
+    }
+}
---
+++ part1/BJ314.java
@@ -0,0 +1,14 @@
+package part1;
+
+public class BJ314 {
+    public static void main(String[] args){
+        double pi = 3.14;
+        int pie = 5;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * pi);
+        System.out.println("パイの半径を倍にします");
+        pi = 10;
+        System.out.println("半径" + pie + "cmのパイの面積は、");
+        System.out.println(pie * pie * pi);
+    }
+}
